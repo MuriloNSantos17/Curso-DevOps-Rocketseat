721e4ff6fd689a361b392765a14d30c97a5a79cb
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2026-09-06 18:34:34 -0300

    create k8s-project

---
+++ projeto-kubernets/app-ts/src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    console.log("ConfigMap",process.env.APP)
+    console.log("Secret",process.env.API_KEY)
+    return 'Hello World!';
+  }
+
+  getExample(): string {
+    return 'Estou rodando no K8S';
+  }
+}
